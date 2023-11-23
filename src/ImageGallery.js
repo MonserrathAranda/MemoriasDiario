74c03fb652d06ff95081c21163424de8242988f4
Suje <11458@utl.edu.mx>
2023-11-22 21:31:40 -0600

    prueba

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -5,14 +5,12 @@ const ImageGallery = () => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    // Lógica para obtener las imágenes al cargar el componente
     fetchImages();
   }, []);
 
   const fetchImages = async () => {
     try {
       const response = await axios.get('http://localhost:5000/imagen');
-      console.log('Datos de la imagen:', response.data.data); 
       setImages(response.data.data);
     } catch (error) {
       console.error('Error fetching images:', error.message);
@@ -23,7 +21,6 @@ const ImageGallery = () => {
     try {
       const response = await axios.delete(`http://localhost:5000/imagen/${id}`);
       console.log(response.data.message);
-      // Actualizar la lista de imágenes después de la eliminación
       fetchImages();
     } catch (error) {
       console.error('Error deleting image:', error.message);
@@ -32,25 +29,24 @@ const ImageGallery = () => {
 
   return (
     <div>
-    <center><h1 style={{ color: 'black' }}>Imagenes</h1></center>
+      <center><h1 style={{ color: 'black' }}>Imagenes</h1></center>
       {images.map((image) => (
         <div key={image.id}>
-        <p>ID: {image.id}</p>
-        <p>Fecha: {image.fecha}</p>
-        <img
-          src={`data:image/jpeg;base64,${image.imagen.toString('base64')}`}
-          alt={`Imagen ${image.id}`}
-          style={{ maxWidth: '50px', height: 'auto' }} 
-        />
-        <button
-          onClick={() => handleDeleteImage(image.id)}
-          style={{ fontSize: '12px', padding: '10px 10px' }} // Ajusta el tamaño del botón
-        >
-          Eliminar
-        </button>
-        <hr />
-      </div>
-      
+          <p>ID: {image.id}</p>
+          <p>Fecha: {image.fecha}</p>
+          <img
+            src={`data:image/jpeg;base64,${btoa(new Uint8Array(image.imagen.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`}
+            alt={`Imagen ${image.id}`}
+            style={{ maxWidth: '50px', height: 'auto' }}
+          />
+          <button
+            onClick={() => handleDeleteImage(image.id)}
+            style={{ fontSize: '12px', padding: '10px 10px' }}
+          >
+            Eliminar
+          </button>
+          <hr />
+        </div>
       ))}
     </div>
   );
--- src/insertImagen.js
+++ src/insertImagen.js
@@ -3,17 +3,21 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
+
 const ImageInsertForm = ({ onClose, onInsert }) => {
   const [fecha, setFecha] = useState(new Date());
   const [selectedImage, setSelectedImage] = useState(null);
+
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     setSelectedImage(file);
   };
 
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
 
     try {
       if (!fecha || !selectedImage) {
@@ -31,6 +35,7 @@ const ImageInsertForm = ({ onClose, onInsert }) => {
         },
       });
 
+
       if (response.data.success) {
         console.log('Imagen insertada exitosamente');
         onInsert(); // Llama a la función proporcionada para actualizar la lista de imágenes
@@ -38,29 +43,29 @@ const ImageInsertForm = ({ onClose, onInsert }) => {
       } else {
         console.error('Error al insertar la imagen:', response.data.error);
       }
-    } catch (error) {
-      console.error('Error en la solicitud:', error.message);
-    }
-  };
 
-  return (
-    <div>
-        <center><h1 style={{ color: 'black' }}>Insertar Imagenes</h1></center>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Fecha:
-          <DatePicker selected={fecha} onChange={(date) => setFecha(date)} />
-        </label>
-        <br />
-        <label>
-          Imagen:
-          <input type="file" accept="image/*" onChange={handleImageChange} />
-        </label>
-        <br />
-        <button type="submit">Insertar Imagen</button>
-      </form>
-    </div>
-  );
+} catch (error) {
+  console.error('Error en la solicitud:', error.message);
+}
+};
+return (
+<div>
+    <center><h1 style={{ color: 'black' }}>Insertar Imagenes</h1></center>
+  <form onSubmit={handleFormSubmit}>
+    <label>
+      Fecha:
+      <DatePicker selected={fecha} onChange={(date) => setFecha(date)} />
+    </label>
+    <br />
+    <label>
+      Imagen:
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+    </label>
+    <br />
+    <button type="submit">Insertar Imagen</button>
+  </form>
+</div>
+);
 };
 
 export default ImageInsertForm;
