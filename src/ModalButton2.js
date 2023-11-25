6a8e92af7b1405a9f0f72ecea1d4e3dafe9914fb
MonserrathAranda <nahomimartinez300@gmail.com>
2023-11-24 18:40:36 -0600

    Se agregan nuevos paquetes
    Se agrega funsion del Button2

--- src/ModalButton2.js
+++ src/ModalButton2.js
@@ -1,11 +1,152 @@
-import React from 'react';
+// ModalButton2.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const ModalButton2 = ({ onClose }) => {
+  const [images, setImages] = useState([]);
+  const [newImage, setNewImage] = useState(null);
+  const [fecha, setFecha] = useState(new Date());
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleImageInsert = async (e) => {
+    e.preventDefault(); 
+
+    try {
+      if (!fecha || !newImage) {
+        console.error('Por favor, completa todos los campos.');
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('fecha', fecha.toISOString());
+      formData.append('imagen', newImage);
+
+      const response = await axios.post('http://localhost:5000/imagen', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      if (response.data.success) {
+        console.log('Imagen insertada exitosamente');
+        fetchData(); // Actualiza la lista de imágenes después de la inserción
+      } else {
+        console.error('Error al insertar la imagen:', response.data.error);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error.message);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/imagen');
+      if (response.data.success) {
+        setImages(response.data.data);
+      } else {
+        console.error('Error al obtener datos de la imagen:', response.data.error);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error.message);
+    }
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handleBack = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const handleImageDelete = async (imageId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5000/imagen/${imageId}`);
+
+      if (response.data.success) {
+        console.log('Imagen eliminada exitosamente');
+        fetchData(); // Actualiza la lista de imágenes después de la eliminación
+      } else {
+        console.error('Error al eliminar la imagen:', response.data.error);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error.message);
+    }
+  };
+
   return (
-    <div className="modal">
+    <div className="modal" style={{ display: 'block' }}>
       <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <p>Hola Maane</p>
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <center>
+          <h1 style={{ color: 'black' }}>Imágenes</h1>
+        </center>
+        <div className="insert-exit-buttons">
+          <form onSubmit={handleImageInsert}>
+            <label>
+              Fecha:
+              <DatePicker selected={fecha} onChange={(date) => setFecha(date)} />
+            </label>
+            <br />
+            <label>
+              Imagen:
+              <input type="file" accept="image/*" onChange={(e) => setNewImage(e.target.files[0])} />
+            </label>
+            <br />
+            <button type="submit">Insertar Imagen</button>
+          </form>
+        </div>
+        <div>
+          <button onClick={handleBack}>Atrás</button>
+          <button onClick={handleNext}>Siguiente</button>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Fecha</th>
+              <th>Imagen</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {images.slice(currentIndex, currentIndex + 2).map((image) => (
+              <tr key={image.id}>
+                <td style={{ display: 'none' }}>{image.id}</td>
+                <td>{image.fecha}</td>
+                <td>
+                  <img
+                    src={`data:image/jpeg;base64,${btoa(
+                      new Uint8Array(image.imagen.data).reduce(
+                        (data, byte) => data + String.fromCharCode(byte),
+                        ''
+                      )
+                    )}`}
+                    alt={`Imagen ${image.id}`}
+                    style={{ maxWidth: '50px', height: 'auto' }}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleImageDelete(image.id)}
+                    style={{ fontSize: '12px', padding: '10px 10px' }}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -162,7 +165,69 @@ app.post('/insertlogin', (req, res) => {
 });
 
 
+// imagen
 
+// Ruta para obtener datos de la tabla "imagen" con enlaces a las imágenes
+app.get('/imagen', (req, res) => {
+  db.query('SELECT id, fecha, imagen FROM imagen', (err, results) => {
+    if (err) {
+      res.send({ success: false, error: err.message });
+    } else {
+      res.send({ success: true, data: results });
+    }
+  });
+});
+
+// Ruta para manejar la carga de imágenes y la inserción en la base de datos
+const storage = multer.memoryStorage(); // Almacena la imagen en memoria como un búfer
+const upload = multer({ storage: storage });
+
+app.post('/imagen', upload.single('imagen'), (req, res) => {
+  try {
+    if (!req.file) {
+      throw new Error('No se proporcionó ninguna imagen.');
+    }
+
+    const fecha = req.body.fecha;
+
+    // Inserta la información en la base de datos
+    db.query(
+      'INSERT INTO imagen (fecha, imagen) VALUES (?, ?)',
+      [fecha, req.file.buffer],
+      (err, result) => {
+        if (err) {
+          throw err;
+        }
+
+        // La imagen se ha cargado con éxito y se ha insertado en la base de datos
+        res.json({ success: true, message: 'Imagen cargada e insertada con éxito' });
+      }
+    );
+  } catch (error) {
+    console.error('Error en la carga de imágenes:', error.message);
+    res.json({ success: false, error: error.message });
+  }
+});
+
+// Ruta para eliminar una imagen por ID
+app.delete('/imagen/:id', (req, res) => {
+  const imageId = req.params.id;
+
+  // Consulta SQL para eliminar la imagen por ID
+  const deleteQuery = 'DELETE FROM imagen WHERE id = ?';
+
+  db.query(deleteQuery, [imageId], (err, result) => {
+    if (err) {
+      res.send({ success: false, error: err.message });
+    } else {
+      if (result.affectedRows > 0) {
+        res.send({ success: true, message: 'Imagen eliminada con éxito' });
+      } else {
+        res.send({ success: false, message: 'No se encontró la imagen para eliminar' });
+      }
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
